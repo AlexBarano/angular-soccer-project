398e70799dcdd495b9a44a9e895d38ae686c304c
Alex Baranov <alexb@moveo.co.il>
2022-03-14 17:32:42 +0200

    fetching data for only one league this time

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -8,23 +8,33 @@ import { MainComponentService } from './main.service';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-  leagues: ILeague[] = [];
-  private maxTeams: number = 5;
+  leaguesUrls = [
+    'https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=English%20Premier%20League',
+    'https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=English%20League%20Championship',
+    'https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=Scottish%20Premier%20League',
+    'https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=German%20Bundesliga',
+    'https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=Italian%20Serie%20A',
+  ];
 
   constructor(private mainComponentService: MainComponentService) {}
 
-  ngOnInit(): void {
-    this.mainComponentService.fetchLeagues().subscribe((fetchedLeagues) => {
-      // let teamsCount: number = 0;
-      // fetchedLeagues.forEach((leagueData) => {
-      //   const league:ILeague = {};
-      //   this.leagues.push(league);
-      //   teamsCount++;
-      //   if (teamsCount > this.maxTeams) {
-      //     return;
-      //   }
-      //});
-      console.log(fetchedLeagues);
-    });
+  ngOnInit(): void {}
+  loadLeagueData(leagueUrl: string) {
+    console.log('loading league data');
+    this.mainComponentService
+      .fetchLeague(leagueUrl)
+      .subscribe((leagueMetaData) => {
+        const league: ILeague = {
+          leagueId: 123,
+          leagueName: '123',
+          teams: [
+            {
+              name: 'asddd',
+              logo: 'fff',
+            },
+          ],
+        };
+        console.log(leagueMetaData);
+      });
   }
 }
