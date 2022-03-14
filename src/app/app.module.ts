d3fa5fc0adb6e76bb82ae8cd0590eb21f9a56bff
Alex Baranov <alexb@moveo.co.il>
2022-03-14 14:28:19 +0200

    Starting over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { DetailsComponent } from './details/details.component';
-import { CardsComponent } from './cards/cards.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardComponent,
-    DetailsComponent,
-    CardsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
